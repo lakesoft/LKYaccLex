0a288c31130cc402c9c99c876298cf68f4646ebd
Hiroshi Hashiguchi <xcatsan@mac.com>
2014-05-05 11:16:59 +0900

    Initial Commit

---
+++ LKYaccLex/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LKYaccLex
+//
+//  Created by Hiroshi Hashiguchi on 2014/05/05.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LKYaccLex/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LKYaccLex
+//
+//  Created by Hiroshi Hashiguchi on 2014/05/05.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
